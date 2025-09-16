9a4b396e251dbd7a4a96bc9833d106c517453178
Ayush Sharma <ayush120410sharma@gmail.com>
2025-09-16 20:40:35 +0530

    Added files

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,7 @@
 //const app_config = { api_url: 'http://192.168.18.15:5000', ip: '192.168.18.15' };
 
 
-let IS_PROD = false
+let IS_PROD = true
 const api_config= IS_PROD
     ? "https://graypoll.onrender.com"
     : "http://localhost:5000";
