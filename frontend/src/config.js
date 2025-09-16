64350ef58964627e87d0c4e4ce32375b8519d45b
Shashank1618 <shashankawasthi426@gmail.com>
2025-09-17 00:05:43 +0530

    Backend server updated

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,10 +1,10 @@
 //const app_config = { api_url: 'http://192.168.18.15:5000', ip: '192.168.18.15' };
 
 
-let IS_PROD = false;
+let IS_PROD = true;
 const app_config = {
   api_url: IS_PROD
-    ? "https://graypoll.onrender.com"
+    ? "https://poll-creator.onrender.com"
     : "http://localhost:5000"
 };
 export default app_config;
